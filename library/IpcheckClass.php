c90c4a204a879afec9c9ae1b881e4f96a0b10633
黄富辉 <huangfuhui@outlook.com>
2017-06-07 12:46:49 +0800

    修改工具入口文件

--- library/IpcheckClass.php
+++ library/IpcheckClass.php
@@ -10,14 +10,23 @@ use Ipcheck\Tool;
 
 class IpcheckClass extends Tool\InitializeClass
 {
+    private $DBHandler = null;            // 数据库操作对象
+
     public function __construct()
     {
+        // 获取数据库操作对象
+        $this->DBHandler = (new DBHandlerFactory())->getDBHandler();
+
         parent::__construct();
     }
 
     public function check()
     {
-        $redis = RedisConnectionSingle::getRedis();
-        var_dump($redis);
+        // 1.记录或更新当前访问者的IP信息
+        $this->DBHandler->recordAccessInfo();
+
+
+        // 关闭当前数据库连接
+        $this->DBHandler->closeConnection();
     }
 }
