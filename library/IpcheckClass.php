adab36feb8bd6d04648d377808d4c16e4222fc4e
黄富辉 <huangfuhui@outlook.com>
2017-06-10 15:47:31 +0800

    连接CheckOption操作

--- library/IpcheckClass.php
+++ library/IpcheckClass.php
@@ -22,9 +22,18 @@ class IpcheckClass extends Tool\InitializeClass
 
     public function check()
     {
-        // 1.记录或更新当前访问者的IP信息
+        // 记录或更新当前访问者的IP信息
         $this->DBHandler->recordAccessInfo();
 
+        // 检测当前IP的访问频率，如果大于用户设置的访问间隔那就采取对应的措施，同时记录访问的有效性
+        $res = $this->DBHandler->checkFrequency();
+        if ($res) {
+            // TODO:返回禁止访问的措施
+
+            $this->DBHandler->recordAccessValidity(false);
+        } else {
+            $this->DBHandler->recordAccessValidity(true);
+        }
 
         // 关闭当前数据库连接
         $this->DBHandler->closeConnection();
