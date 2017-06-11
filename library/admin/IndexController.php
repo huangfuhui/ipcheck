847800deb2df075c01287afbf709a317d33d1669
黄富辉 <huangfuhui@outlook.com>
2017-06-11 18:14:45 +0800

    添加后台首页控制器

---
+++ library/admin/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Ipcheck后台管理入口
+ */
+
+namespace Ipcheck\Admin;
+
+use Ipcheck\Tool;
+
+class IndexController extends Tool\InitializeClass
+{
+    public function index()
+    {
+
+    }
+}
