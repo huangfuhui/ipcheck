11513068da95d8edb3205e2f5c926fe6f30703e2
黄富辉 <huangfuhui@outlook.com>
2017-06-11 18:14:04 +0800

    更新后台管理入口

--- admin.php
+++ admin.php
@@ -2,3 +2,13 @@
 /**
  * ipcheck后台管理入口
  */
+
+// 注册类自动加载器
+require './library/tool/AutoloadClass.php';
+spl_autoload_register('Ipcheck\Tool\AutoloadClass::autoload');
+
+// 加载全局函数
+require './library/common/function.php';
+
+// 准备进入系统管理首页
+(new \Ipcheck\Admin\IndexController())->index();
