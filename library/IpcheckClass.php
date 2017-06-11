0f6b5d8b1890225843f0b8cc14c96156d0f246ab
黄富辉 <huangfuhui@outlook.com>
2017-06-11 17:43:35 +0800

    调用新的监测功能

--- library/IpcheckClass.php
+++ library/IpcheckClass.php
@@ -25,6 +25,9 @@ class IpcheckClass extends Tool\InitializeClass
         // 记录或更新当前访问者的IP信息
         $this->DBHandler->recordAccessInfo();
 
+        // 记录或更新当前访问者的访问次数
+        $this->DBHandler->recordAccessTimes();
+
         // 检测当前IP的访问频率，如果大于用户设置的访问间隔那就采取对应的措施，同时记录访问的有效性
         $res = $this->DBHandler->checkFrequency();
         if ($res) {
