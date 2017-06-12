7bbea7df933dd14678013b3d7c958deca5990973
黄富辉 <huangfuhui@outlook.com>
2017-06-12 23:25:27 +0800

    新增接口内容

--- library/CheckOption.php
+++ library/CheckOption.php
@@ -8,10 +8,26 @@ namespace Ipcheck;
 interface checkOption
 {
     /**
-     * 记录\更新访问者信息
-     * @return bool 成功返回true，失败返回false
+     * 记录或更新当前访问者的信息，按照用户配置只记录最新的N条访问者信息，默认N=200
      */
     function recordAccessInfo();
+
+    /**
+     * 更新当前IP的访问次数
+     */
+    function recordAccessTimes();
+
+    /**
+     * 检测当前IP的访问频率是否符合用户设置的标准
+     * @return bool 是则返回false，否则返回true
+     */
+    function checkFrequency();
+
+    /**
+     * 记录当前IP访问的有效性
+     * @param bool $validity true表示有效，false表示无效
+     */
+    function recordAccessValidity($validity = true);
 
     /**
      * 关闭数据库连接
