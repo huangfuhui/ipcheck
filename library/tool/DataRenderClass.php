3a7bbf17200eda1c7ad2d518eb3c6c0b20f275b4
黄富辉 <huangfuhui@outlook.com>
2017-06-12 23:26:39 +0800

    实现数据渲染工具

--- library/tool/DataRenderClass.php
+++ library/tool/DataRenderClass.php
@@ -5,8 +5,41 @@
 
 namespace Ipcheck\Tool;
 
-
 class DataRenderClass
 {
+    static $ROOT = '';              // 系统根目录
+    static $HTML_PATH = '';         // HTML文件夹
+    static $CSS_PATH = '';          // CSS文件夹
+    static $JS_PATH = '';           // JS文件夹
 
+    public function __construct()
+    {
+        $length = strrpos(__FILE__, 'library' . DIRECTORY_SEPARATOR . 'tool' . DIRECTORY_SEPARATOR . 'DataRenderClass.php');
+        self::$ROOT = substr(__FILE__, 0, $length);
+
+        self::$HTML_PATH = self::$ROOT . 'public' . DIRECTORY_SEPARATOR . 'html' . DIRECTORY_SEPARATOR;
+        self::$CSS_PATH = self::$ROOT . 'public' . DIRECTORY_SEPARATOR . 'css' . DIRECTORY_SEPARATOR;
+        self::$JS_PATH = self::$ROOT . 'public' . DIRECTORY_SEPARATOR . 'js' . DIRECTORY_SEPARATOR;
+    }
+
+    /**
+     * 引入HTML文件
+     * @param string $html 需要引入的HTML文件名，无需后缀，大小写敏感
+     * @param array $htmlData 需要渲染的数据
+     */
+    public function display($html = '', $htmlData = array())
+    {
+        // 空HTML文件则返回指定文件内容
+        if (empty($html)) {
+            require self::$HTML_PATH . 'EMPTY.html';
+            return;
+        }
+
+        $file = self::$HTML_PATH . $html . '.html';
+        if (file_exists($file)) {
+            require $file;
+        } else {
+            require self::$HTML_PATH . 'NOT_FOUND.html';
+        }
+    }
 }
