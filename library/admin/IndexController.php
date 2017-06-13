6745cea547bd7368f0e5911ddcb22d3552647187
黄富辉 <huangfuhui@outlook.com>
2017-06-13 22:52:53 +0800

    更新后台Index控制器内容

--- library/admin/IndexController.php
+++ library/admin/IndexController.php
@@ -3,12 +3,33 @@
  * Ipcheck后台管理入口
  */
 
-namespace Ipcheck\Admin;
+namespace Ipcheck\Admin\Controller;
 
 class IndexController extends BaseController
 {
+    public function __construct()
+    {
+        parent::__construct();
+
+        // 未登录用户或登录过期跳转至登录界面
+        if ($this->action != 'login' && !$this->isLogin) {
+            header('Location:' . $this->ipInfo['SCRIPT_NAME'] . '?a=login');
+        }
+    }
+
+    /**
+     * 后台管理首页
+     */
     public function index()
     {
-        $this->display();
+        $this->display('index');
+    }
+
+    /**
+     * 用户登录页面
+     */
+    public function login()
+    {
+        $this->display('login');
     }
 }
