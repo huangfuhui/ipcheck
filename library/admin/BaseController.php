aeb9002b7f8f73b9c40aab10726ed76d115edbd9
黄富辉 <huangfuhui@outlook.com>
2017-06-13 22:53:15 +0800

    更新基础控制器的一些变量

--- library/admin/BaseController.php
+++ library/admin/BaseController.php
@@ -3,7 +3,7 @@
  * 系统后台基础控制器
  */
 
-namespace Ipcheck\Admin;
+namespace Ipcheck\Admin\Controller;
 
 use Ipcheck\DBHandlerFactory;
 use Ipcheck\Tool\InitializeClass;
@@ -12,10 +12,13 @@ class BaseController extends InitializeClass
 {
     protected $DBHandler = null;                    // 数据库操作对象
 
-    protected $IsLogin = false;                     // 用户登录标记
+    protected $isLogin = false;                     // 用户登录标记
 
-    protected $DataGet = array();                   // 保存GET请求中的参数
-    protected $DataPost = array();                  // 保存POST请求中的参数
+    protected $ipInfo = array();                    // 请求IP信息
+    protected $action = 'index';                    // 请求动作
+
+    protected $dataGet = array();                   // 保存GET请求中的参数
+    protected $dataPost = array();                  // 保存POST请求中的参数
 
     public function __construct()
     {
@@ -25,9 +28,14 @@ class BaseController extends InitializeClass
         $this->DBHandler = (new DBHandlerFactory())->getDBHandler();
 
         // 判断是否登录成功，并标记用户
-        $this->IsLogin = $this->DBHandler->isLogin();
+        $this->isLogin = $this->DBHandler->isLogin();
 
-        $this->DataGet = $_GET;
-        $this->DataPost = $_POST;
+        $this->ipInfo = self::$IPINFOS;
+
+
+        empty($_GET['a']) ? $this->action = 'index' : $this->action = $_GET['a'];
+
+        $this->dataGet = $_GET;
+        $this->dataPost = $_POST;
     }
 }
