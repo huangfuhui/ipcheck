be9950dafca4d1e54c4447a5c795e052f4952eb5
黄富辉 <huangfuhui@outlook.com>
2017-06-14 13:01:22 +0800

    实现login动作

--- library/admin/API/UsrAPI.php
+++ library/admin/API/UsrAPI.php
@@ -9,10 +9,26 @@ class UsrAPI extends BaseAPI
 {
     /**
      * 用户登录API
-     * @return array
      */
     public function login()
     {
-        return $this->result;
+        $usr = $this->dataPost['usr'];
+        $pwd = $this->dataPost['pwd'];
+
+        // 参数判空
+        if (empty($usr) || empty($pwd)) {
+            $this->result['data'] = '账号或密码不能置空！';
+            // 提前返回
+            $this->returnAjax();
+        }
+
+        // 判断身份合法性
+        if ($this->DBHandler->login($usr, $pwd)) {
+            $this->result['code'] = 1;
+            $this->result['data'] = 'admin.php?c=index';
+        } else {
+            $this->result['data'] = '账号或密码错误！';
+        }
+        $this->returnAjax();
     }
 }
