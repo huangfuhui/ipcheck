ca5d417d451b47d57bff2219223bc4653c7c10ab
黄富辉 <huangfuhui@outlook.com>
2017-06-15 22:58:53 +0800

    重写部分后台类加载路径匹配

--- library/tool/AutoloadClass.php
+++ library/tool/AutoloadClass.php
@@ -15,9 +15,9 @@ class AutoloadClass
     public static function autoLoad($class)
     {
         $classInfo = explode('\\', $class);
-        if (in_array('Tool', $classInfo)) {          // 载入 '/library/tool' 文件夹下的工具类
+        if (in_array('Tool', $classInfo)) {                         // 载入 '/library/tool' 文件夹下的工具类
             require __DIR__ . DIRECTORY_SEPARATOR . $classInfo[2] . '.php';
-        } else {                                                     // 载入 '/library' 目录下的类
+        } else {                                                    // 载入 '/library' 目录下的类
             require dirname(__DIR__) . DIRECTORY_SEPARATOR . $classInfo[1] . '.php';
         }
         // TODO:文件 NOT_FOUND 异常处理
@@ -29,9 +29,13 @@ class AutoloadClass
      */
     public static function autoloadForAdmin($class)
     {
+//        echo $class . '<br/>';
+        // TODO:类路径匹配待优化
         $classInfo = explode('\\', $class);
-        if (in_array('Tool', $classInfo)) {         // 载入 '/library/tool' 文件夹下的工具类
+        if (in_array('Tool', $classInfo)) {                         // 载入 '/library/tool' 文件夹下的工具类
             require __DIR__ . DIRECTORY_SEPARATOR . $classInfo[2] . '.php';
+        } elseif (substr_count($class, 'Ipcheck\Install\\')) {      // 载入 '/library/install' 文件夹下的安装器
+            require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'install' . DIRECTORY_SEPARATOR . $classInfo[2] . '.php';
         } elseif (substr_count($class, 'api') ||
             substr_count($class, 'API')
         ) {                                                         // 载入 '/library/admin/API' 文件夹下的API
