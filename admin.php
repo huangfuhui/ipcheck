a1616b1157aa9f444176bb70a4cb91b5039fa758
黄富辉 <huangfuhui@outlook.com>
2017-06-15 23:00:13 +0800

    添加系统初始化安装判断

--- admin.php
+++ admin.php
@@ -10,5 +10,14 @@ spl_autoload_register('Ipcheck\Tool\AutoloadClass::autoloadForAdmin');
 // 加载全局函数
 require './library/common/function.php';
 
+// 执行系统初始化安装
+if (!file_exists(__DIR__ . '/library/install/install.lock')) {
+    $installResult = (new \Ipcheck\Install\InstallOptionClass())->install();
+    if (!$installResult) {
+        echo '数据库安装失败！';
+        exit;
+    }
+}
+
 // 准备进入后台，使用路由工具对请求进行分发
 (new \Ipcheck\Tool\RouterClass())->dispatch();
