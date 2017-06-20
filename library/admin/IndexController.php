1040cf7c40db65a720d31092de1df0d8efda93dc
黄富辉 <huangfuhui@outlook.com>
2017-06-20 23:51:33 +0800

    实现recentRecord方法

--- library/admin/IndexController.php
+++ library/admin/IndexController.php
@@ -22,7 +22,7 @@ class IndexController extends BaseController
      */
     public function index()
     {
-        $this->display('index');
+        $this->recentRecord();
     }
 
     /**
@@ -36,5 +36,88 @@ class IndexController extends BaseController
         }
 
         $this->display('login');
+    }
+
+    /**
+     * 最新记录
+     */
+    public function recentRecord()
+    {
+        // 表头
+        $htmlBody = <<<HTML
+<table>
+    <tr>
+        <th>编号</th>
+        <th>IP地址</th>
+        <th>上次访问时间</th>
+    </tr>
+HTML;
+
+        // 统计最新的IP访问记录数目
+        $list_count = $this->DBHandler->redis->lLen('ip:access_record');
+        if (empty($page) || !is_numeric($page) || $page <= 0) {
+            $page = 0;
+        } else {
+            $page -= 1;
+        }
+
+        // 获取最近访问的IP信息
+        $recent_record = $this->DBHandler->redis->lRange('ip:access_record', $page * 15, ($page + 1) * 15 - 1);
+
+        // 循环输出列表的每一行
+        foreach ($recent_record as $key => $value) {
+            if (++$key % 2 == 0) {
+                $tr_class = 'double_tr';
+            } else {
+                $tr_class = 'single_tr';
+            }
+
+            $id = $key + $page * 15;
+
+            // 将获取到的IP信息从JSON转换成Array，并且格式化目标数据
+            $IPInfo = json_decode($value, true);
+            $IPAddress = $IPInfo['REMOTE_ADDR'];
+            $time = date('Y-m-d H:i:s', $IPInfo['REQUEST_TIME']);
+
+            // 将数据嵌入表格的行元素中
+            $htmlBody .= <<<HTML
+    <tr class="{$tr_class}">
+        <td>{$id}</td>
+        <td>{$IPAddress}</td>
+        <td>{$time}</td>
+    </tr>
+HTML;
+        }
+
+        // 表结尾
+        $htmlBody .= <<<HTML
+</table>
+HTML;
+
+        $this->display('index', array('html_body' => $htmlBody));
+    }
+
+    /**
+     * 总体记录
+     */
+    public function totalRecord()
+    {
+
+    }
+
+    /**
+     * 访问统计
+     */
+    public function accessCount()
+    {
+
+    }
+
+    /**
+     * 访问禁止
+     */
+    public function banRecord()
+    {
+
     }
 }
