577a4e62b90e19db05855239c60250a80bba267f
huangfuhui <huangfuhui@outlook.com>
2017-08-09 09:43:58 +0800

    增加自动装载函数的注销

--- admin.php
+++ admin.php
@@ -3,12 +3,15 @@
  * ipcheck后台管理入口
  */
 
+// 根目录
+define('ROOT_PATH', dirname(__FILE__));
+
 // 注册类自动加载器
-require './library/tool/AutoloadClass.php';
+require ROOT_PATH . '/library/tool/AutoloadClass.php';
 spl_autoload_register('Ipcheck\Tool\AutoloadClass::autoloadForAdmin');
 
 // 加载全局函数
-require './library/common/function.php';
+require ROOT_PATH . '/library/common/function.php';
 
 // 执行系统初始化安装
 if (!file_exists(__DIR__ . '/library/install/install.lock')) {
@@ -21,3 +24,6 @@ if (!file_exists(__DIR__ . '/library/install/install.lock')) {
 
 // 准备进入后台，使用路由工具对请求进行分发
 (new \Ipcheck\Tool\RouterClass())->dispatch();
+
+// 注销类自动加载器
+spl_autoload_unregister('Ipcheck\Tool\AutoloadClass::autoload');
--- check.php
+++ check.php
@@ -3,12 +3,18 @@
  * ipcheck工具
  */
 
+// 根目录
+define('ROOT_PATH', dirname(__FILE__));
+
 // 注册类自动加载器
-require './library/tool/AutoloadClass.php';
+require ROOT_PATH . '/library/tool/AutoloadClass.php';
 spl_autoload_register('Ipcheck\Tool\AutoloadClass::autoload');
 
 // 加载全局函数
-require './library/common/function.php';
+require ROOT_PATH . '/library/common/function.php';
 
 // 执行IP监测过滤
 (new Ipcheck\IpcheckClass())->check();
+
+// 注销类自动加载器
+spl_autoload_unregister('Ipcheck\Tool\AutoloadClass::autoload');
