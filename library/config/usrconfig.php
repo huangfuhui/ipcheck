c99dff92704a0372ddea99bdc79b26f7b96ccd19
huangfuhui <huangfuhui@outlook.com>
2017-08-12 21:55:35 +0800

    新增拦截器工具以及对应的配置文件

--- library/config/usrconfig.php
+++ library/config/usrconfig.php
@@ -4,8 +4,43 @@
  */
 
 return array(
-    // 配置系统访问规则
+    // 配置系统基本访问规则(BaseAccessRule)
     'AccessFrequency' => 10,                    // 单位时间内允许访问次数，默认'10'
     'TimeUnit' => 'm',                          // 时间单位，可选：d-天 h-时 m-分，不区分大小写，默认'm'
+    'RequestRedirect' => '',                    // 非法请求重定向至第三方网站，为空则忽略该操作
     'NewRecords' => 200,                        // 保留N条最新访问IP的详细信息，默认200条
+
+    // 配置拦截器(Interceptor)
+    'UseInterceptor' => true,                   // 默认启动拦截器
+    'AutoAddToBlackList' => false,              // 是否自动将拦截下来的IP加入黑名单
+    'RuleChain' => array(                       // 过滤规则链，可以多条规则配合使用，默认使用'RuleA+RuleB+RuleC'
+        'RuleA',
+        'RuleB',
+        'RuleC',
+    ),
+
+    // 配置过滤规则(FilterRules)
+    'RuleA' => array(                           // 规则A，单位时间内触发基本访问规则(BaseAccessRule)N次，则触发拦截器
+        'AccessFrequency' => 10,                // 单位时间内允许触发系统基本访问规则(BaseAccessRule)的次数，默认'5'
+        'TriggerTimeUnit' => 'h',               // 时间单位，可选：d-天 h-时，不区分大小写，默认'h'
+    ),
+    'RuleB' => array(                           // 规则B，单位时间内访问次数超过一定数量则触发拦截器
+        'AccessFrequency' => 100,               // 单位时间内访问次数，默认'100'
+        'TriggerTimeUnit' => 'h',               // 时间单位，可选：d-天 h-时，不区分大小写，默认'h'
+    ),
+    'RuleC' => array(                           // 规则C，单位时间内某一受保护的URL访问次数超过一定数量则触发拦截器
+        'AccessFrequency' => 3,                 // 单位时间内访问次数，默认'3'
+        'TriggerTimeUnit' => 'm',               // 时间单位，可选：d-天 h-时 m-分，不区分大小写，默认'm'
+        'URLS' => array(                        // 受保护的URL，可以配置多个，不必配置主机域名部分
+            '/example.php',                     // 拦截器会拦截：http://hostname/example.php、hostname/example.php?a=hello类型的请求
+        ),
+    ),
+    'RuleD' => array(                           // 规则D，非白名单内的IP访问受保护的URL则触发拦截器
+        'WhiteList' => array(                   // 白名单，可配置多个IP
+            '127.0.0.1',
+        ),
+        'URLS' => array(                        // 受保护的URL，可配置多个，不必配置主机域名部分
+            '/example.php',                     // 拦截器会拦截非白名单内的IP访问：http://hostname/example.php、hostname/example.php?a=hello类型的URL
+        ),
+    )
 );
---
+++ library/tool/InterceptorClass.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * 拦截器
+ */
+
+namespace Ipcheck\Tool;
+
+class InterceptorClass
+{
+    public $ipInfo = array();                   // 请求IP的信息
+    public $DBHandler = null;                   // 数据库操作对象
+
+    public $useInterceptor = true;              // 是否启用拦截器
+    public $autoAddToBlackList = false;         // 是否自动加入黑名单
+    public $ruleChain = array(                  // 过滤规则链
+        'RuleA',
+        'RuleB',
+        'RuleC',
+    );
+
+    public function __construct($ipInfo, $DBHandler)
+    {
+        $this->ipInfo = $ipInfo;
+        $this->DBHandler = $DBHandler;
+
+        $this->useInterceptor = getConf('UseInterceptor');
+        $this->autoAddToBlackList = getConf('AutoAddToBlackList');
+        $this->ruleChain = getConf('RuleChain');
+    }
+
+    /**
+     * 拦截不符合规则的IP，拦截成功则返回true
+     * @return bool
+     */
+    public function Filter()
+    {
+        if (empty($ipInfo) || !$this->useInterceptor) {
+            return false;
+        }
+
+        $res = false;
+        foreach ($this->filterRules as $rule) {
+            $res = $this->DBHandler->interceptor($rule);
+            if ($res) {
+                // TODO:记录拦截信息
+                break;
+            }
+        }
+        return $res;
+    }
+}
