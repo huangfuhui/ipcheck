9b254214f592fa199fc07e850d137ebacf474f7a
huangfuhui <huangfuhui@outlook.com>
2017-09-14 12:39:16 +0800

    表格工具

---
+++ library/tool/TableClass.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * 表格工具，用于生成表格、渲染数据
+ *
+ * User: huangfuhui
+ * Date: 2017/9/14
+ * Email: huangfuhui@outlook.com
+ */
+
+namespace Ipcheck\Tool;
+
+
+class TableClass
+{
+    private $th = array();              // 表头
+    private $data = array();            // 数据
+
+    private $table = '';                // 表格HTML
+
+    public function __construct($th, $data = array())
+    {
+        $this->th = $th;
+        $this->data = $data;
+    }
+
+    public function generateTable()
+    {
+        // 表格头部
+        $dataHead = '<tr>';
+        foreach ($this->th as $head) {
+            $dataHead .= '<th>' . $head . '</th>' . PHP_EOL;
+        }
+        $dataHead .= '</tr>';
+
+        // 表格数据
+        $dataRows = '';
+        foreach ($this->data as $data) {
+            $dataRows .= '<tr>';
+            foreach ($data as $k => $v) {
+                $dataRows .= '<td>' . $v . '</td>' . PHP_EOL;
+            }
+            $dataRows .= '</tr>';
+        }
+
+        $this->table = <<<HTML
+<table>
+    $dataHead
+    $dataRows
+</table>
+HTML;
+        return $this->table;
+    }
+}
